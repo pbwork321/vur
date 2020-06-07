2e046fd4c024e09b81a0ecaa285cbd65db16ecd7
paras <bhardwajparas58@gmail.com>
2020-06-07 22:48:42 +0530

    first commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <div
+      class="flex bg-gray-200"
+    >Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maxime ad sint fuga repellendus blanditiis nam corporis vero rem. Laudantium nemo error provident beatae voluptate blanditiis, itaque reiciendis voluptas magnam doloremque?</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App"
+};
+</script>
+
+<style>
+</style>
